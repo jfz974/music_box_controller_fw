9782abb476b060d745aa9d2caa04a8d8c8e24ce4
julien F <j.fezandelle@gmail.com>
2026-07-19 13:17:52 +0200

    button matrix templated

--- src/button_matrix.h
+++ src/button_matrix.h
@@ -6,14 +6,48 @@
 
 #include "pico/stdlib.h"
 
+template <uint ColumnCount, uint RowCount>
 class ButtonMatrix {
 public:
-	static constexpr uint kColumnCount = 4;
-	static constexpr uint kRowCount = 4;
+	static constexpr uint kColumnCount = ColumnCount;
+	static constexpr uint kRowCount = RowCount;
 
-	ButtonMatrix(const uint (&output_pins)[kColumnCount], const uint (&input_pins)[kRowCount]);
+	ButtonMatrix(const uint (&output_pins)[kColumnCount], const uint (&input_pins)[kRowCount]) {
+		static_assert(kColumnCount * kRowCount <= 32, "ButtonMatrix bitfield exceeds uint32_t");
 
-	uint32_t read_state();
+		for (uint column = 0; column < kColumnCount; ++column) {
+			output_pins_[column] = output_pins[column];
+			gpio_init(output_pins_[column]);
+			gpio_set_dir(output_pins_[column], GPIO_OUT);
+			gpio_put(output_pins_[column], 0);
+		}
+
+		for (uint row = 0; row < kRowCount; ++row) {
+			input_pins_[row] = input_pins[row];
+			gpio_init(input_pins_[row]);
+			gpio_set_dir(input_pins_[row], GPIO_IN);
+			gpio_pull_down(input_pins_[row]);
+		}
+	}
+
+	uint32_t read_state() {
+		uint32_t state = 0;
+
+		for (uint column = 0; column < kColumnCount; ++column) {
+			gpio_put(output_pins_[column], 1);
+			sleep_us(50);
+
+			for (uint row = 0; row < kRowCount; ++row) {
+				if (gpio_get(input_pins_[row]) != 0) {
+					state |= 1u << (row * kColumnCount + column);
+				}
+			}
+
+			gpio_put(output_pins_[column], 0);
+		}
+
+		return state;
+	}
 
 private:
 	std::array<uint, kColumnCount> output_pins_;
--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@ int main() {
 	const uint column_pins[4] = {2, 3, 4, 5};
 	const uint row_pins[4] = {6, 7, 8, 9};
 	uint32_t last_state = 0;
+	using Matrix4x4 = ButtonMatrix<4, 4>;
 
 	stdio_init_all();
-	ButtonMatrix button_matrix(column_pins, row_pins);
+	Matrix4x4 button_matrix(column_pins, row_pins);
 
 	printf("4x4 matrix ready: cols GP2-5, rows GP6-9\r\n");
 
@@ -17,9 +18,9 @@ int main() {
 		uint32_t state = button_matrix.read_state();
 		uint32_t new_presses = state & ~last_state;
 
-		for (uint row = 0; row < ButtonMatrix::kRowCount; ++row) {
-			for (uint column = 0; column < ButtonMatrix::kColumnCount; ++column) {
-				uint32_t mask = 1u << (row * ButtonMatrix::kColumnCount + column);
+		for (uint row = 0; row < Matrix4x4::kRowCount; ++row) {
+			for (uint column = 0; column < Matrix4x4::kColumnCount; ++column) {
+				uint32_t mask = 1u << (row * Matrix4x4::kColumnCount + column);
 				if ((new_presses & mask) != 0) {
 					printf("matrix[%u][%u]\r\n", row, column);
 				}
