f1911f2f3058e8e5152a8a89e4adaeb9ec5f946e
julien F <j.fezandelle@gmail.com>
2026-07-19 13:07:37 +0200

    add simple button logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,23 @@
 #include "pico/stdlib.h"
 
 int main() {
+	const uint button_pin = 15;
+
 	stdio_init_all();
+	gpio_init(button_pin);
+	gpio_set_dir(button_pin, GPIO_IN);
+	gpio_pull_up(button_pin);
+
+	bool last_pressed = gpio_get(button_pin) == 0;
+	printf("button ready on GP%u\r\n", button_pin);
 
 	while (true) {
-		printf("music_box_controller_fw alive\r\n");
-		sleep_ms(1000);
+		bool pressed = gpio_get(button_pin) == 0;
+		if (pressed != last_pressed) {
+			printf("button %s\r\n", pressed ? "pressed" : "released");
+			last_pressed = pressed;
+		}
+
+		sleep_ms(20);
 	}
 }
