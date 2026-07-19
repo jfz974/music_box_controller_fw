5f92e2666fee58edc0a15c3610fe0e11f7620ab4
julien F <j.fezandelle@gmail.com>
2026-07-19 13:14:22 +0200

    create button matrix

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,41 @@
 #include "pico/stdlib.h"
 
 int main() {
-	const uint button_pin = 15;
+	const uint column_pins[4] = {2, 3, 4, 5};
+	const uint row_pins[4] = {6, 7, 8, 9};
+	bool last_pressed[4][4] = {};
 
 	stdio_init_all();
-	gpio_init(button_pin);
-	gpio_set_dir(button_pin, GPIO_IN);
-	gpio_pull_up(button_pin);
+	for (uint column = 0; column < 4; ++column) {
+		gpio_init(column_pins[column]);
+		gpio_set_dir(column_pins[column], GPIO_OUT);
+		gpio_put(column_pins[column], 0);
+	}
 
-	bool last_pressed = gpio_get(button_pin) == 0;
-	printf("button ready on GP%u\r\n", button_pin);
+	for (uint row = 0; row < 4; ++row) {
+		gpio_init(row_pins[row]);
+		gpio_set_dir(row_pins[row], GPIO_IN);
+		gpio_pull_down(row_pins[row]);
+	}
+
+	printf("4x4 matrix ready: cols GP2-5, rows GP6-9\r\n");
 
 	while (true) {
-		bool pressed = gpio_get(button_pin) == 0;
-		if (pressed != last_pressed) {
-			printf("button %s\r\n", pressed ? "pressed" : "released");
-			last_pressed = pressed;
+		for (uint column = 0; column < 4; ++column) {
+			gpio_put(column_pins[column], 1);
+			sleep_us(50);
+
+			for (uint row = 0; row < 4; ++row) {
+				bool pressed = gpio_get(row_pins[row]) != 0;
+				if (pressed && !last_pressed[row][column]) {
+					printf("matrix[%u][%u]\r\n", row, column);
+				}
+				last_pressed[row][column] = pressed;
+			}
+
+			gpio_put(column_pins[column], 0);
 		}
 
-		sleep_ms(20);
+		sleep_ms(5);
 	}
 }
