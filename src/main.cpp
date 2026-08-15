911abefdd93e368043f43ade11423e11a73879b3
julien F <j.fezandelle@gmail.com>
2026-08-15 19:37:54 +0200

    Add PixelLEd

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "button_matrix.h"
 #include "pico/stdlib.h"
+#include "ws2812_strip.h"
 
 int main() {
 	const uint column_pins[4] = {2, 3, 4, 5};
@@ -12,7 +13,17 @@ int main() {
 	stdio_init_all();
 	Matrix4x4 button_matrix(column_pins, row_pins);
 
+	Ws2812Strip<16> led_strips[3] = {
+		Ws2812Strip<16>(10),
+		Ws2812Strip<16>(11),
+		Ws2812Strip<16>(12),
+	};
+	for (auto& strip : led_strips) {
+		strip.show();
+	}
+
 	printf("4x4 matrix ready: cols GP2-5, rows GP6-9\r\n");
+	printf("3x WS2812 16-pixel strips ready: GP10, GP11, GP12\r\n");
 
 	while (true) {
 		uint32_t state = button_matrix.read_debounced_state();
